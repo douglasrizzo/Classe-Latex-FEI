56b72935f9e2d38fbb5c639b5f2e58558119b5b2
heliohenriquevillanueva@gmail.com <helio@note.note>
2015-08-07 20:47:56 -0300

    remocao arquivos extras

--- prerequisitos_fedora22.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-dnf install -y texlive texlive-babel-portuges.noarch texlive-morewrites.noarch texlive-algorithm2e.noarch texlive-thmtools.noarch texlive-tocloft.noarch texlive-imakeidx.noarch texlive-glossaries.noarch texlive-abntex2.noarch 
